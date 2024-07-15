106b9dd42def443045a7d5004f4e855188d845f1
migara tennakoon <migaraten@gmail.com>
2024-07-15 19:19:04 +0530

    add aos animation

--- src/components/sectionTwo/sectionTwo.jsx
+++ src/components/sectionTwo/sectionTwo.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CarouselComponent from "./carousel";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function SectionTwo() {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div>
       <div class="grid grid-cols-2 gap-4 py-16">
         <div className="px-16">
           <div>
-            <p className="text-[50px] font-black">WHAT IS BEPPE?</p>
-            <p className="text-[20px]">
-              Immerse yourself in the mystical world of 'Ethereal Dreams,' a
-              mesmerizing digital artwork that transcends the boundaries of
-              reality and fantasy. This NFT captures the essence of otherworldly
-              beauty, with intricate details and vibrant colors that evoke a
-              sense of wonder and enchantment. Own a piece of the digital realm
-              and let 'Ethereal Dreams' inspire your imagination.
-            </p>
+            <div
+              data-aos="fade-right"
+              data-aos-easing="linear"
+              data-aos-duration="1500"
+            >
+              <p className="text-[50px] font-black">WHAT IS BEPPE?</p>
+              <p className="text-[20px]">
+                Immerse yourself in the mystical world of 'Ethereal Dreams,' a
+                mesmerizing digital artwork that transcends the boundaries of
+                reality and fantasy. This NFT captures the essence of
+                otherworldly beauty, with intricate details and vibrant colors
+                that evoke a sense of wonder and enchantment. Own a piece of the
+                digital realm and let 'Ethereal Dreams' inspire your
+                imagination.
+              </p>
+            </div>
           </div>
         </div>
 
-        <div className="px-8">
+        <div
+          className="px-8"
+          data-aos="fade-left"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+        >
           <CarouselComponent />
         </div>
       </div>
