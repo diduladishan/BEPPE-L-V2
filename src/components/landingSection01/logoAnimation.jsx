b289c1c9009621dc8ca2a3efb96c28d544498a03
diduladishan <diduladishan14@gmail.com>
2024-07-16 13:57:19 +0530

    update responsiveness of the animated text bar

--- src/components/landingSection01/logoAnimation.jsx
+++ src/components/landingSection01/logoAnimation.jsx
@@ -5,8 +5,10 @@ import right from "../assets/landingSection01/right.png";
 
 const MovingText = () => {
   const textArray = new Array(30).fill(
-    <div className="flex items-center bg-[#000] border-x-8 border-[#fff] px-1">
-      <p className="font-bold text-[28px] text-white px-6 py-5">BEPPE</p>
+    <div className="flex items-center bg-[#000] 2xl:border-x-8 xl:border-x-8 lg:border-x-7 md:border-x-6 sm:border-x-5 border-x-4 border-[#fff] px-1">
+      <p className="font-bold 2xl:text-[28px] xl:text-[26px] lg:text-[24px] md:text-[22px] sm:text-[20px] text-white px-6 2xl:py-5 xl:py-5 lg:py-4 md:py-4 sm:py-3 py-2">
+        BEPPE
+      </p>
     </div>
   );
 
