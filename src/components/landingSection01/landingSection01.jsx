cced9ffa20fa1431c66171a070539d6ef952b76f
Dominic Brian <dominic91960@gmail.com>
2024-07-29 15:16:29 +0530

    Added landing page animations.

--- src/components/landingSection01/landingSection01.jsx
+++ src/components/landingSection01/landingSection01.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import { React, useEffect } from "react";
+import AOS from "aos";
+
+import "aos/dist/aos.css";
 import backgroundImage from "../assets/landingSection01/backgroundImagesWithBranchers.png";
 import MovingText from "./logoAnimation";
 
@@ -8,9 +11,23 @@ import bottomBranch from "../assets/landingSection01/bottom-branch.png";
 import leftBranchGroup from "../assets/landingSection01/leftBranchGroup.png";
 import rightBranchGroup from "../assets/landingSection01/rightBranchGroup.png";
 
+import boatBackground from "../assets/romeo/boat-background.png";
+import maleCharacter from "../assets/romeo/male-character.png";
+import femaleCharacter from "../assets/romeo/female-character.png";
+import leftGrass from "../assets/romeo/left-grass.png";
+import rightGrass from "../assets/romeo/right-grass.png";
+import upperGrass from "../assets/romeo/upper-grass.png";
+import topic from "../assets/romeo/topic.png";
+
 import "./test.css";
 
 function LandingSection01() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
     <div>
       {/* <div>
@@ -24,7 +41,7 @@ function LandingSection01() {
         </div>
       </div> */}
 
-      <div className="">
+      <div className="relative -z-20">
         {/* <div className="absolute top-[-60px] right-0">
           <img src={topBranch} alt="opensea icon" className="w-full" />
         </div> */}
@@ -40,8 +57,48 @@ function LandingSection01() {
         <div className="absolute bottom-[70px] right-0">
           <img src={rightBranchGroup} alt="opensea icon" className="w-full" />
         </div> */}
-
-        <img src={backgroundImage} alt="background image" className="" />
+        <div
+          className="absolute top-0 right-0"
+          data-aos="fade-right"
+          data-aos-duration="2000"
+          data-aos-delay="2000"
+        >
+          <img src={maleCharacter} alt="opensea icon" className="w-full" />
+        </div>
+        <div
+          className="absolute top-0 right-0"
+          data-aos="fade-left"
+          data-aos-duration="2000"
+          data-aos-delay="2000"
+        >
+          <img src={femaleCharacter} alt="opensea icon" className="w-full" />
+        </div>
+        <div
+          className="absolute top-0 right-0"
+          data-aos="slide-right"
+          data-aos-delay="500"
+        >
+          <img src={leftGrass} alt="opensea icon" className="w-full" />
+        </div>
+        <div
+          className="absolute top-0 right-0"
+          data-aos="slide-left"
+          data-aos-delay="1000"
+        >
+          <img src={rightGrass} alt="opensea icon" className="w-full" />
+        </div>
+        <div className="absolute top-0 right-0" data-aos="slide-down">
+          <img src={upperGrass} alt="opensea icon" className="w-full" />
+        </div>
+        <div
+          className="absolute top-0 right-0"
+          data-aos="zoom-out"
+          data-aos-duration="2500"
+          data-aos-delay="3000"
+        >
+          <img src={topic} alt="opensea icon" className="w-full" />
+        </div>
+        <img src={boatBackground} alt="background image" />
       </div>
 
       <MovingText />
